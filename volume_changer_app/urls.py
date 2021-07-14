f229f74ac401cdaae694b6da22bf4d7542766b0d
ritnaro yoshikawa <ittanmomennimakaretyaimashita@gmail.com>
2021-07-15 06:38:26 +0900

    Added file download view.

--- volume_changer_app/urls.py
+++ volume_changer_app/urls.py
@@ -5,4 +5,6 @@ app_name = 'vca'
 
 urlpatterns = [
     path('', views.index, name='index'),            
+    path('download/<int:pk>/', views.download, name='download'),
+    path('download_list/', views.download_list, name='download_list'),
 ]
--- volume_changer_app/views.py
+++ volume_changer_app/views.py
@@ -1,18 +1,45 @@
-from django.shortcuts import render, redirect
+import logging
+import mimetypes
+import shutil
+
+from django.http import HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
 
 from .forms import UploadMusicForm
+from .models import Music
 
 def index(request):
     context = {}
     if request.method == 'POST':
         form = UploadMusicForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
-            return redirect('vca:download')
+            # music modelにuploadされたfileのnameを代入
+            music = form.save(commit=False)
+            music.name = form.cleaned_data['file'].name
+            music.save()
+            request.session['uploaded_music_pk'] = music.pk
+            return redirect('vca:download_list')
     else:
         form = UploadMusicForm()
     context['form'] = form
     return render(request, 'volume_changer_app/index.html', context)
 
-def download(request):
-    return render(request, 'volume_changer_app/download.html')
+def download(request, pk):
+    """clickでdownloadを実行"""
+    uploaded_music = get_object_or_404(Music, pk=pk) 
+    filename = uploaded_music.name
+    guessed_type = mimetypes.guess_type(filename)[0]
+    response = HttpResponse(content_type=guessed_type or 'application/octet-stream')
+    response['Content-Disposition'] = f'attachment; filename={filename}' # enforce download
+    shutil.copyfileobj(uploaded_music.file, response) # copy file to response
+    return response
+
+def download_list(request):
+    """downloadできるfileのlist"""
+    # server側でdonwload機能を実装するのがいいのか?
+    context = {}
+    uploaded_music_pk = request.session.get('uploaded_music_pk')
+    if uploaded_music_pk:
+        uploaded_music = Music.objects.get(pk=uploaded_music_pk)
+        context['uploaded_music'] = uploaded_music
+    return render(request, 'volume_changer_app/download_list.html', context)
