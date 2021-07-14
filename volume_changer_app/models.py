c088b7b2b0c8d3ea7353d961bf2c1f69e424cf56
ritnaro yoshikawa <ittanmomennimakaretyaimashita@gmail.com>
2021-07-15 06:20:51 +0900

    Added name field to Music model.

--- volume_changer_app/models.py
+++ volume_changer_app/models.py
@@ -3,3 +3,4 @@ from django.db import models
 class Music(models.Model):
     # file will be uploaded to MEDIA_ROOT/before_conversion/
     file = models.FileField(upload_to='before_conversion/')
+    name = models.CharField(max_length=255)
