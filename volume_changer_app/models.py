adde5c323308ecdd2dfe8225361b5e02efd9a3b7
rintaro yoshikawa <ittanmomennimakaretyaimashita@gmail.com>
2021-09-07 07:27:54 +0900

    file upload先をvolume_changer_app/settings.pyで管理

--- volume_changer_app/models.py
+++ volume_changer_app/models.py
@@ -5,7 +5,7 @@ from . import settings
 class Music(models.Model):
     # file will be uploaded to MEDIA_ROOT/before_conversion/
     file = models.FileField(
-            upload_to='before_conversion/',
+            upload_to=settings.UPLOAD_TO,
             validators=[FileValidator( 
                 max_size=settings.MAX_SIZE,
                 content_types=settings.ALLOWED_CONTENT_TYPES)],
--- volume_changer_app/settings.py
+++ volume_changer_app/settings.py
@@ -1,3 +1,5 @@
+UPLOAD_TO = 'before_conversion'
+
 # for Music Model validator 
 MAX_SIZE = 5*1024**2 # 10MB
 ALLOWED_CONTENT_TYPES = ('audio/mpeg', 'audio/wav')
